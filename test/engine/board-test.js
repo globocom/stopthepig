5a57d467cb1e694a605c28c08676e1d22984c23c
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2018-08-09 17:14:57 -0300

    tests: remove unnecessary tests

--- test/engine/board-test.js
+++ test/engine/board-test.js
@@ -4,47 +4,22 @@ import Board from './../../engine/board'
 
 describe('Board', () => {
   describe('initialize', () => {
+    let board
+
     before(() => {
-      this.board = new Board()
+      board = new Board()
     })
 
-    it('with a width of 7', () => {
-      assert.equal(this.board.width, 7)
+    it('with a width of 11', () => {
+      assert.equal(board.width, 11)
     })
 
-    it('with a height of 6', () => {
-      assert.equal(this.board.height, 6)
+    it('with a height of 11', () => {
+      assert.equal(board.height, 11)
     })
 
-    it('with maxMoves of 42', () => {
-      assert.equal(this.board.maxMoves, 42)
-    })
-  })
-
-  describe('push', () => {
-    beforeEach(() => {
-      this.board = new Board()
-    })
-
-    it('add a piece to the board', () => {
-      const player = {
-        'char': 'x'
-      }
-      this.board.push(player, 0)
-      assert.equal(this.board.matrix[0][5], 'x')
-    })
-  })
-
-  describe('available position', () => {
-    beforeEach(() => {
-      this.board = new Board()
-    })
-
-    it('show all columns available', () => {
-      assert.equal(
-        this.board.getAvailableColumns().length,
-        this.board.width
-      )
+    it('with maxMoves of 121', () => {
+      assert.equal(board.maxMoves, 121)
     })
   })
 })
