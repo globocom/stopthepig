eb7ee17026b6f9b3d9d276dcc0adb93e7b1b8b83
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2018-08-09 17:14:19 -0300

    refactor: remove unused methods and logic

--- engine/board.js
+++ engine/board.js
@@ -1,27 +1,9 @@
 class Board {
   constructor() {
-    this.width = 7
-    this.height = 6
+    this.width = 11
+    this.height = 11
     this.maxMoves = this.width * this.height
     this.matrix = this.buildMatrix()
-  }
-
-  push(player, column) {
-    const row = this.matrix[column].lastIndexOf(null)
-
-    if (row < 0) return false
-
-    this.matrix[column][row] = player.char
-
-    return [column, row]
-  }
-
-  cloneBoard() {
-    const clone = []
-    for (const column in this.matrix) {
-      clone.push(this.matrix[column].slice(0))
-    }
-    return clone
   }
 
   buildMatrix() {
@@ -35,38 +17,6 @@ class Board {
     }
 
     return matrix
-  }
-
-  getAvailableColumns() {
-    const positions = []
-
-    for (const column in this.matrix) {
-      if (this.matrix[column].lastIndexOf(null) >= 0)
-        positions.push(parseInt(column))
-    }
-
-    return positions
-  }
-
-  draw() {
-    for (const row = 0; row < this.height; row++) {
-      for (const column = 0; column < this.width; column++) {
-        const cell = this.matrix[column][row] || ' '
-        process.stdout.write("| " + cell + " ")
-      }
-      process.stdout.write("\n")
-    }
-  }
-
-  getAvailableColumns() {
-    const positions = []
-
-    for (const column in this.matrix) {
-      if (this.matrix[column].lastIndexOf(null) >= 0)
-        positions.push(parseInt(column))
-    }
-
-    return positions
   }
 
   draw() {
