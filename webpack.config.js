38f96559921d3b62a137dceb3d3edb7333a3120f
Rodrigo Willrich <rodrigo.willrich@hotmail.com>
2018-08-09 17:17:35 -0300

    Create 'watch' task

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const {
+  NODE_ENV = 'development'
+} = process.env
+
 module.exports = {
+  mode: NODE_ENV,
   entry: './client/app',
   module: {
     rules: [
