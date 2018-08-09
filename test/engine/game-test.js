83ae0d7e00d5fb15eb53a97eb416b4a45c62a52c
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2018-08-09 17:15:55 -0300

    refactor: remove unnecessary game engine logics

---
+++ test/engine/game-test.js
@@ -0,0 +1,32 @@
+import assert from 'assert'
+
+import Game from './../../engine/game'
+import Board from './../../engine/board'
+
+describe('Game', () => {
+  describe('initialize', () => {
+    let game
+    let pig
+    let farmer
+
+    before(() => {
+      game = new Game(pig, farmer)
+    })
+
+    it('should initialize with no winner', () => {
+      assert.equal(game.winner, null)
+    })
+
+    it('should initialize with a valid Board', () => {
+      assert.equal(game.board.constructor, Board)
+    })
+
+    it('should initialize with two valid players', () => {
+      assert.equal(game.players.length, 2)
+    })
+
+    it('should initialize with no moves', () => {
+      assert.equal(game.moves.length, 0)
+    })
+  })
+})
