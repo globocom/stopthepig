a6429146ff7025e5845332f04c9acc02b18dcb1b
Alexandre Mantovani Tavares <alexandre@live.in>
2018-08-09 17:54:03 -0300

    refactor: move getAvailablePositions function to game

--- engine/board.js
+++ engine/board.js
@@ -34,39 +34,6 @@ class Board {
     }
   }
 
-  getAvailablePositions() {
-    var currentPosition = this.pig;
-    if (currentPosition.row === 0 ||
-      currentPosition.row === this.columns ||
-      currentPosition.col === 0 ||
-      currentPosition.col === this.rows) {
-      return { winGame: false, loseGame: true };
-    }
-    var availableLocations = []
-    if (currentPosition.row % 2) {
-      availableLocations = [{ row: currentPosition.row - 1, col: currentPosition.col },
-      { row: currentPosition.row - 1, col: currentPosition.col + 1 },
-      { row: currentPosition.row, col: currentPosition.col - 1 },
-      { row: currentPosition.row, col: currentPosition.col + 1 },
-      { row: currentPosition.row + 1, col: currentPosition.col },
-      { row: currentPosition.row + 1, col: currentPosition.col + 1 }
-      ];
-    }
-    else {
-      availableLocations = [{ row: currentPosition.row - 1, col: currentPosition.col - 1 },
-      { row: currentPosition.row - 1, col: currentPosition.col },
-      { row: currentPosition.row, col: currentPosition.col - 1 },
-      { row: currentPosition.row, col: currentPosition.col + 1 },
-      { row: currentPosition.row + 1, col: currentPosition.col - 1 },
-      { row: currentPosition.row + 1, col: currentPosition.col }
-      ];
-    }
-    console.log(availableLocations)
-    availableLocations.forEach(e => {
-      this.matrix[e.row][e.col] = "*"
-    })
-  }
-
 }
 
 export default Board
