136f8a52dc54d18e22bfd9807b1d63ff749f37af
Alexandre Mantovani Tavares <alexandre@live.in>
2018-08-09 17:20:25 -0300

    feat: check available positions

--- engine/board.js
+++ engine/board.js
@@ -1,18 +1,18 @@
 class Board {
-  constructor() {
-    this.width = 11
-    this.height = 11
-    this.maxMoves = this.width * this.height
+  constructor(pig) {
+    this.columns = 11
+    this.rows = 11
     this.matrix = this.buildMatrix()
+    this.pig = pig
   }
 
   buildMatrix() {
     const matrix = []
 
-    for (let i = 0; i < this.width; i++) { // columns
-      matrix[i] = []
-      for (let j = 0; j < this.height; j++) { // rows
-        matrix[i][j] = null
+    for (let x = 0; x < this.rows; x++) {
+      matrix[x] = []
+      for (let y = 0; y < this.columns; y++) {
+        matrix[x][y] = null
       }
     }
 
@@ -20,14 +20,48 @@ class Board {
   }
 
   draw() {
-    for (const row = 0; row < this.height; row++) {
-      for (const column = 0; column < this.width; column++) {
+    for (let row = 0; row < this.rows; row++) {
+      for (let column = 0; column < this.columns; column++) {
         const cell = this.matrix[column][row] || ' '
         process.stdout.write("| " + cell + " ")
       }
       process.stdout.write("\n")
     }
   }
+
+  getAvailablePositions() {
+    var currentPosition = this.pig;
+    if (currentPosition.row === 0 ||
+      currentPosition.row === this.columns ||
+      currentPosition.col === 0 ||
+      currentPosition.col === this.rows) {
+      return { winGame: false, loseGame: true };
+    }
+    var availableLocations = []
+    if (currentPosition.row % 2) {
+      availableLocations = [{ row: currentPosition.row - 1, col: currentPosition.col },
+      { row: currentPosition.row - 1, col: currentPosition.col + 1 },
+      { row: currentPosition.row, col: currentPosition.col - 1 },
+      { row: currentPosition.row, col: currentPosition.col + 1 },
+      { row: currentPosition.row + 1, col: currentPosition.col },
+      { row: currentPosition.row + 1, col: currentPosition.col + 1 }
+      ];
+    }
+    else {
+      availableLocations = [{ row: currentPosition.row - 1, col: currentPosition.col - 1 },
+      { row: currentPosition.row - 1, col: currentPosition.col },
+      { row: currentPosition.row, col: currentPosition.col - 1 },
+      { row: currentPosition.row, col: currentPosition.col + 1 },
+      { row: currentPosition.row + 1, col: currentPosition.col - 1 },
+      { row: currentPosition.row + 1, col: currentPosition.col }
+      ];
+    }
+    console.log(availableLocations)
+    availableLocations.forEach(e => {
+      this.matrix[e.row][e.col] = "*"
+    })
+  }
+
 }
 
 export default Board
