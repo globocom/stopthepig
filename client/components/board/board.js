ca46147b5b2c5adb0640a2b9a5e19d0f93e39ff8
Ícaro Heimig <icaro.heimig@corp.globo.com>
2018-08-10 15:04:31 -0300

    fix layout

--- client/components/board/board.js
+++ client/components/board/board.js
@@ -7,19 +7,7 @@ import unitBuild from '../../../unity/Build/test_communication.json'
 
 import { PIG_FLYING_IMAGE } from '../../constants'
 
-
-const message = { board: 9, 'initial_state': [{ x: 3, 'y': 3 }, { 'x': 3, 'y': 4 }, { x: 3, y: 5 }, { x: 4, y: 3 }, { 'x': 5, 'y': 3 }, { x: 5, y: 4 }, { 'x': 5, 'y': 5 }], moves: [{ player: 'P', 'action': 'move', 'x': 5, 'y': 5 }, { 'player': 'F', 'action': 'block', x: 10, 'y': 10 }, { player: 'P', action: 'move', 'x': 6, y: 5 }, { player: 'F', 'action': 'block', x: 10, y: 9 }, { player: 'P', 'action': 'move', x: 7, 'y': 5 }, { 'player': 'F', action: 'block', 'x': 10, y: 8 }, { 'player': 'P', 'action': 'move', 'x': 8, y: 5 }, { player: 'F', action: 'block', x: 10, y: 7 }, { player: 'P', action: 'move', 'x': 9, y: 5 }, { player: 'F', action: 'block', 'x': 10, 'y': 6 }, { player: 'P', action: 'finish', 'x': 10, y: 5 }], winner: 'P', 'score': { P: { 'score': 5 }, 'F': { score: 5 } } };
 let instance = null
-
-const sendMessage = (instance, message) => {
-  instance.SendMessage(
-    'GameManager',
-    'GameRenderer',
-    JSON.stringify(message)
-  )
-}
-
-console.log(unitBuild)
 
 const initUnit = () => {
   instance = UnityLoader.instantiate(
@@ -32,11 +20,11 @@ const initUnit = () => {
 
 const Board = () => (
   <div className="Board">
-    <div id="gameContainer"></div>
-    <img src={PIG_FLYING_IMAGE} alt="Pig Loader" />
+    <div id="gameContainer" style={{ height: '100%' }}></div>
   </div>
 )
 
-initUnit();
+initUnit()
+window.instance = instance
 
 export default Board
--- client/pages/home.js
+++ client/pages/home.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import sampleFarmerAlgorithm from '!raw-loader!../../engine/algorithms/sample-farmer-algorithm'
 import samplePigAlgorithm from '!raw-loader!../../engine/algorithms/sample-pig-algorithm'
+import pigImage from '!file-loader!../../docs/logo.png'
 
 import Editor from '../components/editor'
 import Board from '../components/board'
 
 import './home.css'
 
+const movePig = () => {
+  const message = { board: 11, 'initial_state': [{ x: 3, 'y': 3 }, { 'x': 3, 'y': 4 }, { x: 3, y: 5 }, { x: 4, y: 3 }, { 'x': 5, 'y': 3 }, { x: 5, y: 4 }, { 'x': 5, 'y': 5 }], moves: [{ player: 'P', 'action': 'move', 'x': 5, 'y': 5 }, { 'player': 'F', 'action': 'block', x: 10, 'y': 10 }, { player: 'P', action: 'move', 'x': 6, y: 5 }, { player: 'F', 'action': 'block', x: 10, y: 9 }, { player: 'P', 'action': 'move', x: 7, 'y': 5 }, { 'player': 'F', action: 'block', 'x': 10, y: 8 }, { 'player': 'P', 'action': 'move', 'x': 8, y: 5 }, { player: 'F', action: 'block', x: 10, y: 7 }, { player: 'P', action: 'move', 'x': 9, y: 5 }, { player: 'F', action: 'block', 'x': 10, 'y': 6 }, { player: 'P', action: 'finish', 'x': 10, y: 5 }], winner: 'P', 'score': { P: { 'score': 5 }, 'F': { score: 5 } } };
+  const sendMessage = (instance, message) => {
+    instance.SendMessage(
+      'GameManager',
+      'GameRenderer',
+      JSON.stringify(message)
+    )
+  }
+  sendMessage(window.instance, message)
+}
+
 const Home = () => (
   <main>
+    <header>
+      <img src={pigImage} alt="Logo" />
+    </header>
+
     <Board />
+
+    <button
+      className="Button"
+      onClick={() => movePig()}>TESTAR</button>
 
     <section className="Algorithms-area">
 
